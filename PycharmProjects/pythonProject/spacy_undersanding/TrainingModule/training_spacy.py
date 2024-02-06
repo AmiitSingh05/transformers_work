f42962618c15868aa6801ac57a5eaa8865c41b48
amiitsingh5 <singh.amiit05@gmail.com>
2024-02-06 09:10:00 +0530

    removed the print statements

--- PycharmProjects/pythonProject/spacy_undersanding/TrainingModule/training_spacy.py
+++ PycharmProjects/pythonProject/spacy_undersanding/TrainingModule/training_spacy.py
@@ -9,24 +9,15 @@ from sentence_generator import list_of_sentences
 #     # Add more examples with your name
 # ]
 train_data = list_of_sentences
-#print("list_of_sentences type :", type(list_of_sentences))
-#print(" type ", type(train_data))
 nlp = spacy.load("en_core_web_sm")
-#print(nlp.pipe_names, type(nlp.pipe_names[5]))
 
 # Create a blank NER model
 ner = nlp.get_pipe("ner")
 
 # Add a new label to the NER model
 ner.add_label("PERSON")
-# Update the NER model with the training data
-#print(list_of_sentences)
-# for text, annotations in train_data:
-#     print(type(text), type(annotations))
-# for text, annotations in list_of_sentences:
-#     print(type(text), type(annotations))
-# print(train_data)
 
+# Update the NER model with the training data
 for text, annotations in list_of_sentences:
     example = Example.from_dict(nlp.make_doc(text), annotations)
     ner.update([example], drop=0.5)
